e8d80f31d786438dc3048a7bcef3e07bb0bda45a
青柳奏汰 <greenwillow49@gmail.com>
2023-09-14 17:50:07 +0900

    [Fix]users#convertToCustomFormat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
-    def show
-        user = User.find(params[:id])
-        render json: convertToCustomFormat(user)
-    end
+  def show
+    user = User.find(params[:id])
+    render json: convertToCustomFormat(user)
+  end
 
-    private
+  private
 
-    def convertToCustomFormat(user)
-        customData =
-        {
-          id: user.id,
-          name: user.name,
-          birthday: user.birthday,
-          introduction: user.introduction
-          profileImageUrl: user.profile_image.url
-        }
-        return customData
-    end
+  def convertToCustomFormat(user)
+    customData =
+    {
+      id: user.id,
+      name: user.name,
+      birthday: user.birthday,
+      introduction: user.introduction,
+      profileImageUrl: user.profile_image.url
+    }
+    return customData
+  end
 end
